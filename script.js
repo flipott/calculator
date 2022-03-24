8ed25ef2c17988e967e3ee65212d9dfc90bbc97d
flipott <ottphillipa@gmail.com>
2022-03-24 12:46:21 -0400

    Added keypress functionality and fixed small bugs

--- script.js
+++ script.js
@@ -11,18 +11,20 @@ let operator = null;
 let opFlag = false;
 let inputHold = null;
 
-buttons.addEventListener('click', buttonClick)
+buttons.addEventListener('click', buttonClick);
+document.addEventListener('keydown', keyButton);
 
 function buttonClick(e) {
-
-    const isButton = e.target.nodeName === 'BUTTON';
-    
-    if (!isButton) {
-      return;
+    if (!e.target) {
+        currentInput = e.value;
+    } else {
+        const isButton = e.target.nodeName === 'BUTTON';
+        if (!isButton) {
+          return;
+        };
+        currentInput = e.target.value;
     }
-
-    currentInput = e.target.value;
-
+    
     if (opFlag == true && currentInput != "del" && currentInput != "=" && firstValue != null || display.textContent == "DIV/0 ERROR") {
         operationClear();
     }
@@ -30,77 +32,70 @@ function buttonClick(e) {
     if (currentInput == 'clr') {
         clear();
     } else if (currentInput == 'del' && runningValue == false && (display.textContent != "NaN" && (operators.includes(inputHold) == false))) {
-        display.textContent = display.textContent.slice(0, -1)
+        display.textContent = display.textContent.slice(0, -1);
     } else {
-        if (currentInput != "del" && currentInput != ".") {
+        if (currentInput != "del" && currentInput != "." && (display.textContent.length < 12)) {
             display.textContent += currentInput;
             if (currentInput != "=") {
                 inputHold = currentInput;
             }
         } else if (currentInput == ".") {
-            if (display.textContent.includes(".") == false) {
+            if (display.textContent.includes(".") == false && (display.textContent.length < 12)) {
                 display.textContent += currentInput;
-            }
-        }
-    }
+            };
+        };
+    };
 
     displayLength(display.textContent);
 
     if (currentInput == '=') {
         equals();
     } else if (operators.includes(currentInput)) {
-        
         inputHold = currentInput;
-        
         if (operators.includes(display.textContent.slice(-1))) {
             display.textContent = display.textContent.slice(0, -1);
         }
 
-
         if (firstValue == null) {
-            e.target.style.backgroundColor = 'darkgray'
-
+            document.querySelectorAll(`[value = "${currentInput}"]`)[0].style.backgroundColor = "darkgray";
             firstValue = display.textContent;
             displayLength(display.textContent);
-            
+        
             if (isNaN(firstValue) || firstValue == '') {
-                e.target.style.backgroundColor = 'lightgray'
+                document.querySelectorAll(`[value = "${currentInput}"]`)[0].style.backgroundColor = "lightgray";
                 return clear()
-            }
-
+            };
             opFlag = true;
             operator = currentInput;
 
         } else {
                 colorClear()
-                e.target.style.backgroundColor = 'darkgray'
+                document.querySelectorAll(`[value = "${currentInput}"]`)[0].style.backgroundColor = "darkgray";
                 runningTotal();
                 displayLength(display.textContent);
-
         };
-    } 
-};
+    };
+}
 
 
 //Calculates total when the equals button is pressed
 function equals() {
-
-
-
     //Returns original input if operator then "=" is selected
     if (operators.includes(inputHold)) {
         display.textContent = display.textContent.slice(0,-1);
         colorClear();
         return;
-    }
+    };
 
     if (firstValue != null) {
         if (operator != null) {
             display.textContent = display.textContent.slice(0, -1);
             secondValue = display.textContent;
-            if (secondValue == 0 && operator == "/") {
+
+            if (parseInt(secondValue) === 0 && operator == "/") {
                 return divZero();
             }
+
             total = operate(operator, firstValue, secondValue);
             display.textContent = total;
             displayLength(display.textContent);
@@ -130,7 +125,7 @@ function runningTotal() {
     if (runningValue == false) {
         secondValue = display.textContent;
 
-        if (secondValue == 0 && operator == "/") {
+        if (parseInt(secondValue) === 0 && operator == "/") {
             return divZero();
         }
  
@@ -161,6 +156,8 @@ function runningTotal() {
     }
 }
 
+
+//Returns an error if the user tries to divide by 0
 function divZero() {
     display.textContent = "DIV/0 ERROR"
     firstValue = null;
@@ -195,8 +192,7 @@ function operationClear() {
 
 //Stops display from exceeding 14 characters
 function displayLength(string) {
-    if (string.length > 14) {
-        // display.textContent = string.slice(0, -1);
+    if (string.length > 12) {
         display.textContent = string.substring(0,13);
     }
 }
@@ -204,19 +200,19 @@ function displayLength(string) {
 //Basic math functions
 function add(a,b) {
     return a+b;
-};
+}
 
 function subtract(a,b) {
     return a-b;
-};
+}
 
 function multiply(a,b) {
     return a*b;
-};
+}
 
 function divide(a,b) {
     return a/b;
-};
+}
 
 function operate(operator, a, b) {
     a = parseFloat(a);
@@ -230,18 +226,103 @@ function operate(operator, a, b) {
     } else if (operator === "/") {
         return numRound(divide(a,b));
     };
-};
+}
 
+//Resets colors once numbers are calculated
 function colorClear() {
-
     for (item of opButtons) {
         item.style.backgroundColor = 'lightgrey';
     }
 }
 
 function numRound(numStr) {
-    roundNum = parseFloat(numStr)
+    roundNum = parseFloat(numStr);
     return parseFloat(roundNum.toFixed(7));
 }
 
-
+//Binds keypresses to button clicks
+function keyButton(e) {
+    switch (e.keyCode) {
+        case 48:
+        case 96:
+            buttonClick(document.getElementById('zero'));
+            break;
+        case 49:
+        case 97:
+            buttonClick(document.getElementById('one'));
+            break;
+        case 50:
+        case 98:
+            buttonClick(document.getElementById('two'));
+            break;
+        case 51:
+        case 99:
+            buttonClick(document.getElementById('three'));
+            break;
+        case 52:
+        case 100:
+            buttonClick(document.getElementById('four'));
+            break;
+        case 53:
+        case 101:
+            buttonClick(document.getElementById('five'));
+            break;
+        case 54:
+        case 102:
+            buttonClick(document.getElementById('six'));
+            break;
+        case 55:
+        case 103:
+            buttonClick(document.getElementById('seven'));
+            break;
+        case 56:
+        case 104:
+            if (e.shiftKey === true) {
+                buttonClick(document.getElementById('multiply'));
+                break;
+            } else {
+                buttonClick(document.getElementById('eight'));
+                break;}
+        case 57:
+        case 105:
+            buttonClick(document.getElementById('nine'));
+            break;
+        case 27:
+            buttonClick(document.getElementById('clear'));
+            break;
+        case 8:
+        case 46:
+            buttonClick(document.getElementById('del'));
+            break;
+        case 191:
+        case 111:
+            buttonClick(document.getElementById('divide'));
+            break;
+        case 88:
+        case 106:
+            buttonClick(document.getElementById('multiply'));
+            break;
+        case 173:
+        case 109:
+            buttonClick(document.getElementById('subtract'));
+            break;
+        case 61:
+            if (e.shiftKey === true) {
+                buttonClick(document.getElementById('plus'));
+                break;
+            } else {
+                buttonClick(document.getElementById('equals'));
+                break;
+            }
+        case 107:
+            buttonClick(document.getElementById('plus'));
+            break;
+        case 13:
+            buttonClick(document.getElementById('equals'));
+            break;
+        case 190:
+        case 110:
+            buttonClick(document.getElementById('decimal'));
+            break;
+    };
+}
